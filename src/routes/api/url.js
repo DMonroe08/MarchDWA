341b654348646dd1d164c8296ff6dcb0fc67ad6c
Danielle Monroe <dmonroe08@fullsail.edu>
2017-03-02 18:11:22 -0500

    Fixes issue with shortener function

--- src/routes/api/url.js
+++ src/routes/api/url.js
@@ -1,7 +1,8 @@
+//exports what the router comes up with
 module.exports = (express) => {
   const router = express.Router();
 
-router.post('/urls', (rep, res) => {
+router.post('/urls', (req, res) => {
   var short = require('../shortener');
   res.json({original: req.body.shortened,
     shortened: short.shortenURL()});
--- src/routes/shortener.js
+++ src/routes/shortener.js
@@ -1,7 +1,10 @@
+
+//Function to create a random string 
 function shortenURL(){
 var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 var text = "";
-for (var i = 0; i < length; i++){
+var range = 8;
+for (var i = 0; i < range; i++){
   text += string.charAt(Math.floor(Math.random() * string.length));
 };
 return text;
